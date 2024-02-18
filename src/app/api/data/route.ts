3a29b387582a888af963c9c1dae5dae32716b99a
Bartosz Art <bartosz.art@gmail.com>
2024-02-19 00:09:55 +0100

    Testing vercol logs on getData

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -6,6 +6,8 @@ export async function GET(req: any) {
 	const { searchParams } = new URL(req.url);
 	const itemId: string | null = searchParams.get('itemId');
 
+	console.log(`Get item id: ${itemId}`);
+
 	if (!itemId) return NextResponse.json({}, { status: 400 });
 
 	const items: Data[] = await getData(Number(itemId));
