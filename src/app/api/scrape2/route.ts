bffa57e0f1efe42100e6ac8f6ed7f845bdef6a3e
Bartosz Art <bartosz.art@gmail.com>
2024-02-19 17:59:08 +0100

    Still testing

--- src/app/api/scrape2/route.ts
+++ src/app/api/scrape2/route.ts
@@ -1,7 +1,15 @@
+import Data from '@/models/Data';
+import { getData } from '@/repository/postgres/dataRepository';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: any) {
-	console.log(`Start test`);
+	console.log(`Test get`);
+	const { searchParams } = new URL(req.url);
+	const itemId: string | null = searchParams.get('itemId');
 
-	return NextResponse.json({}, { status: 200 });
+	if (!itemId) return NextResponse.json({}, { status: 400 });
+
+	const items: Data[] = await getData(Number(itemId));
+
+	return NextResponse.json({ items }, { status: 200 });
 }
