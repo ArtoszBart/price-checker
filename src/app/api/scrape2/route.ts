6cfb6034e608001b395373ddffc2688e6a21e25a
Bartosz Art <bartosz.art@gmail.com>
2024-02-19 18:08:04 +0100

    Still testing

--- src/app/api/scrape2/route.ts
+++ src/app/api/scrape2/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: any) {
 	console.log(`Test get`);
-	const { searchParams } = new URL(req.url);
 
 	return NextResponse.json({ asd: 'ok' }, { status: 200 });
 }
