ba497c6d24f81c9c7d9ce1978b4a7b6906092d62
Bartosz Art <bartosz.art@gmail.com>
2024-02-19 18:01:15 +0100

    Still testing

--- src/app/api/scrape2/route.ts
+++ src/app/api/scrape2/route.ts
@@ -9,7 +9,5 @@ export async function GET(req: any) {
 
 	if (!itemId) return NextResponse.json({}, { status: 400 });
 
-	const items: Data[] = await getData(Number(itemId));
-
-	return NextResponse.json({ items }, { status: 200 });
+	return NextResponse.json({ asd: 'ok' }, { status: 200 });
 }
